44e0b47a19e5f54889f2abba50d3d8b7c7324410
Alecto Irene Perez <perez.cs@pm.me>
2022-02-15 16:05:36 -0700

    Add test case where enum values are printed out
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- tests/test_nav.cpp
+++ tests/test_nav.cpp
@@ -1,3 +1,45 @@
+#include <nav/nav.hpp>
 #include <catch2/catch_test_macros.hpp>
+#include <fmt/core.h>
 
-TEST_CASE("Hello Tests") {}
+enum nav_declare_enum(
+    MyEnum, // Name
+    int,    // Base type
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H,
+    I,
+    J,
+    K,
+    L,
+    M,
+    N,
+    O = -15,
+    P,
+    Q,
+    R,
+    S,
+    T,
+    U,
+    V,
+    W,
+    X,
+    Y,
+    Z, );
+
+TEST_CASE("Hello Tests") {
+    fmt::print("names.size(): {}\n", nav::enum_traits<MyEnum>::names.size());
+    using traits = nav::enum_traits<MyEnum>;
+
+    for (size_t i = 0; i < traits::count; i++) {
+        fmt::print(
+            "Name: '{}', Value: '{}'\n",
+            traits::names[i],
+            int(traits::values[i]));
+    }
+}
