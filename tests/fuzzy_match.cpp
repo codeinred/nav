a83af7418793ce08f011d470395b99db9b790357
Alecto Irene Perez <perez.cs@pm.me>
2022-02-17 16:17:17 -0700

    Add tests for fuzzy matching
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

---
+++ tests/fuzzy_match.cpp
@@ -0,0 +1,81 @@
+#include <nav/nav.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <fmt/core.h>
+#include <random>
+#include <string>
+
+// clang-format off
+nav_declare_enum(
+    CommonWords, // Name of enum
+    int,         // Base type
+    // A h*ck-ton of words varying in length from 5-7 leters
+    About, Search, Other, Which, Their, There, Contact, Online, First, Would,
+    These, Click, Service, Price, People, State, Email, Health, World, Music,
+    Should, Product, System, Policy, Number, Please, Support, Message, After,
+    Video, Where, Rights, Public, Books, School, Through, Links, Review, Years,
+    Order, Privacy, Items, Company, Group, Under, General, January, Reviews,
+    Program, Games, Could, Great, United, Hotel, Center, Store, Travel, Report,
+    Member, Details, Terms, Before, Hotels, Right, Because, Local, Those,
+    Using, Results, Office, Design, Posted, Address, Within, States, Phone,
+    Subject, Between, Forum, Family, Based, Black, Check, Special, Prices,
+    Website, Index, Being, Women, Today, South, Project, Pages, Version,
+    Section, Found, Sports, House, Related, County, Photo, Members, Power,
+    While, Network, Systems, Three, Total, Place, Without, Access, Think,
+    North, Current, Posts, Media, Control, Water, History, Since, Guide, Board,
+    Change, White, Small, Rating, During, Return, Account, Times, Sites,
+    Level, Digital, Profile, Events, Hours, Image, Title, Another, Shall,
+    Class, Still, Money, Quality, Every, Listing, Content, Country, Private,
+    Little, Visit, Tools, Reply, Compare, Movies, Include, College, Value,
+    Article, Provide, Source, Author, Press, Learn, Around, Print, Course,
+    Canada, Process, Stock, Credit, Point, Science, Sales, English, Estate,
+    Select, Windows, Photos, Thread, Large, Gallery, Table, However, October,
+    Market, Library, Really, Action, Start, Series, Model, Human, Second,
+    Movie, Forums, March, Better, Yahoo, Going, Medical, Friend, Server, Study,
+    Staff, Again, Looking, Issues, April, Never, Users, Street, Topic, Comment,
+    Things, Working, Against, Person, Below, Mobile, Party, Payment, Login,
+    Student, Offers, Legal, Above, Recent, Stores, Problem, Memory, Social,
+    August, Quote, Story, Options, Rates, Create, Young, America, Field, Paper,
+    Single, Example, Girls, Latest, Changes, Night, Texas, Poker, Status,
+    Browse, Issue, Range, Seller, Court, );
+// clang-format on
+
+TEST_CASE("Fuzzy matching enum names", "[find_fuzzy]") {
+    using traits = nav::enum_traits<CommonWords>;
+
+    REQUIRE(traits::values[traits::find_fuzzy("abutt")] == CommonWords::About);
+
+    enum EditKind { Insert, Delete, Substitute };
+    std::mt19937_64 rng;
+
+    std::uniform_int_distribution<char> letter_dist('a', 'z');
+    std::uniform_int_distribution<> edit_dist(0, 2);
+    for (size_t i = 0; i < traits::count; i++) {
+        std::string_view name = traits::names[i];
+        auto str = std::string(name);
+        std::uniform_int_distribution<size_t> index_dist(0, name.size() - 1);
+
+        int index = index_dist(rng);
+        switch (EditKind(edit_dist(rng))) {
+            case Insert:
+                str.insert(str.begin() + index, letter_dist(rng));
+                break;
+            case Delete: str.erase(str.begin() + index); break;
+            case Substitute: str[index] = letter_dist(rng); break;
+        }
+        size_t found_i = traits::find_fuzzy(str);
+        INFO(fmt::format(
+            "Search string: '{}'\n"
+            "Original name: '{}'\n"
+            "Found name:    '{}'\n"
+            "Dist between search and original: {}\n"
+            "Dist between search and discovered: {}\n",
+            str,
+            name,
+            traits::names[found_i],
+            traits::distance_metric(name, str),
+            traits::distance_metric(traits::names[found_i], str)));
+
+        REQUIRE(traits::distance_metric(str, name) <= 1);
+        REQUIRE(traits::distance_metric(str, traits::names[found_i]) <= 1);
+    }
+}
