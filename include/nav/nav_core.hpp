df32c5db41fb1bac97a4ffa9f3789deacd9aac07
Alecto Irene Perez <perez.cs@pm.me>
2022-02-24 23:59:57 -0700

    Split lowercase into nav/nav_lowercase.hpp
    This helps keep nav_core clean
    
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- include/nav/nav_core.hpp
+++ include/nav/nav_core.hpp
@@ -298,44 +298,6 @@ struct enum_name_list : enum_type_info<Enum> {
 };
 
 template <class Enum>
-struct lowercase_enum_name_list : enum_type_info<Enum> {
-    using block_type = typename enum_name_list<Enum>::block_type;
-
-   private:
-    constexpr static block_type name_block {[](block_type& dest) {
-        block_type const& source = enum_name_list<Enum>::get_name_block();
-        {
-            size_t N = source.block_size();
-            for (size_t i = 0; i < N; i++) {
-                char ch = source.data[i];
-                dest.data[i] = 'A' <= ch && ch <= 'Z' ? ch - 'A' + 'a' : ch;
-            }
-        }
-        {
-            size_t N = source.size();
-            for (size_t i = 0; i <= N; i++) {
-                dest.offsets[i] = source.offsets[i];
-            }
-        }
-    }};
-
-   public:
-    using iterator = detail::string_block_iterator;
-    using const_iterator = iterator;
-    constexpr iterator begin() const noexcept {
-        return name_block.begin();
-    }
-    constexpr iterator end() const noexcept {
-        return name_block.end();
-    }
-    constexpr std::string_view operator[](size_t i) const noexcept {
-        auto off1 = name_block.offsets[i];
-        auto off2 = name_block.offsets[i + 1] - 1;
-        return std::string_view(name_block.data + off1, off2 - off1);
-    }
-};
-
-template <class Enum>
 constexpr bool is_nav_enum = enum_type_info<Enum>::is_nav_enum;
 
 template <class Enum>
@@ -346,9 +308,6 @@ constexpr enum_value_list<Enum> enum_values {};
 
 template <class Enum>
 constexpr enum_name_list<Enum> enum_names {};
-
-template <class Enum>
-constexpr lowercase_enum_name_list<Enum> lowercase_enum_names {};
 } // namespace nav
 
 #define nav_declare_enum(EnumType, BaseType, ...)                              \
---
+++ tests/nav_lowercase.cpp
@@ -0,0 +1,72 @@
+#include <catch2/catch_test_macros.hpp>
+#include <nav/nav_lowercase.hpp>
+#include <fmt/core.h>
+
+nav_declare_enum(
+    RainbowColors,
+    int,
+    Red = 0xff0000,
+    // Orange is a mix of Red and Yellow (#ffff00)
+    Orange = (Red + 0xffff00) / 2 & 0xffff00,
+    Yellow = 0xffff00,
+    // Green is Yellow, without the Red
+    Green = Yellow - Red,
+    Blue = 0x0000ff,
+    Indigo = 0x8A2BE2,
+    // We're representing violet as Red + Blue
+    Violet = Red + Blue);
+
+
+TEST_CASE("Traits on lowercase", "") {
+    INFO(
+        "lowercase_enum_name_list<Enum> is a stateless type that provides a "
+        "way to get and iterate over the names of an enumeration in lowercase");
+    REQUIRE(std::is_empty_v<nav::lowercase_enum_name_list<RainbowColors>>);
+    REQUIRE(std::is_trivial_v<nav::lowercase_enum_name_list<RainbowColors>>);
+}
+
+TEST_CASE("Check lowercase names", "[lowercase]") {
+    auto names = nav::lowercase_enum_names<RainbowColors>;
+    REQUIRE(names[0] == "red");
+    REQUIRE(names[1] == "orange");
+    REQUIRE(names[2] == "yellow");
+    REQUIRE(names[3] == "green");
+    REQUIRE(names[4] == "blue");
+    REQUIRE(names[5] == "indigo");
+    REQUIRE(names[6] == "violet");
+}
+
+TEST_CASE("Iterate over lowercase names", "[lowercase]") {
+    auto names = nav::lowercase_enum_names<RainbowColors>;
+    auto values = nav::enum_values<RainbowColors>;
+
+    std::string_view reference_names[] {
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "indigo",
+        "violet"};
+
+    size_t index = 0;
+    for (std::string_view name : names) {
+        REQUIRE(name == reference_names[index++]);
+    }
+}
+
+TEST_CASE("Ensure lowercase names end with null terminator", "[lowercase]") {
+    auto names = nav::lowercase_enum_names<RainbowColors>;
+
+    for (std::string_view name : names) {
+        char const* c_str = name.data();
+        size_t len = name.size();
+        INFO(fmt::format(
+            "len = {},\n"
+            "c_str = \"{}\"\n"
+            "(Obtained from nav::lowercase_enum_names)",
+            len,
+            c_str));
+        REQUIRE(c_str[len] == '\0');
+    }
+}
