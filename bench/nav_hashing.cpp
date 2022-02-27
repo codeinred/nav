27b0acbdb2d0d52ca89e0cfa8783d9a4f4c5d2d2
Alecto Irene Perez <perez.cs@pm.me>
2022-02-27 16:21:37 -0700

    Add benchmarks for hashing
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- bench/nav_hashing.cpp
+++ bench/nav_hashing.cpp
@@ -1,1 +1,56 @@
 #include <benchmark/benchmark.h>
+#include <nav/nav_hashmap.hpp>
+#include <utility>
+
+using benchmark::State;
+
+template <class Fn>
+void hash_string(State& state, Fn func, std::string_view sv) {
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(sv);
+        auto result = func(sv);
+        benchmark::DoNotOptimize(result);
+    }
+}
+
+using nav::detail::hash_sv;
+constexpr auto std_hash_sv = std::hash<std::string_view> {};
+
+constexpr std::string_view source = "0123456789abcdef0123456789abcdef";
+
+BENCHMARK_CAPTURE(hash_string, (hash, 0), hash_sv, source.substr(0, 0));
+BENCHMARK_CAPTURE(hash_string, (hash, 1), hash_sv, source.substr(0, 1));
+BENCHMARK_CAPTURE(hash_string, (hash, 2), hash_sv, source.substr(0, 2));
+BENCHMARK_CAPTURE(hash_string, (hash, 3), hash_sv, source.substr(0, 3));
+BENCHMARK_CAPTURE(hash_string, (hash, 4), hash_sv, source.substr(0, 4));
+BENCHMARK_CAPTURE(hash_string, (hash, 5), hash_sv, source.substr(0, 5));
+BENCHMARK_CAPTURE(hash_string, (hash, 6), hash_sv, source.substr(0, 6));
+BENCHMARK_CAPTURE(hash_string, (hash, 7), hash_sv, source.substr(0, 7));
+BENCHMARK_CAPTURE(hash_string, (hash, 8), hash_sv, source.substr(0, 8));
+BENCHMARK_CAPTURE(hash_string, (hash, 9), hash_sv, source.substr(0, 9));
+BENCHMARK_CAPTURE(hash_string, (hash, 10), hash_sv, source.substr(0, 10));
+BENCHMARK_CAPTURE(hash_string, (hash, 11), hash_sv, source.substr(0, 11));
+BENCHMARK_CAPTURE(hash_string, (hash, 12), hash_sv, source.substr(0, 12));
+BENCHMARK_CAPTURE(hash_string, (hash, 13), hash_sv, source.substr(0, 13));
+BENCHMARK_CAPTURE(hash_string, (hash, 14), hash_sv, source.substr(0, 14));
+BENCHMARK_CAPTURE(hash_string, (hash, 15), hash_sv, source.substr(0, 15));
+BENCHMARK_CAPTURE(hash_string, (hash, 16), hash_sv, source.substr(0, 16));
+
+
+BENCHMARK_CAPTURE(hash_string, (std_hash, 0), std_hash_sv, source.substr(0, 0));
+BENCHMARK_CAPTURE(hash_string, (std_hash, 1), std_hash_sv, source.substr(0, 1));
+BENCHMARK_CAPTURE(hash_string, (std_hash, 2), std_hash_sv, source.substr(0, 2));
+BENCHMARK_CAPTURE(hash_string, (std_hash, 3), std_hash_sv, source.substr(0, 3));
+BENCHMARK_CAPTURE(hash_string, (std_hash, 4), std_hash_sv, source.substr(0, 4));
+BENCHMARK_CAPTURE(hash_string, (std_hash, 5), std_hash_sv, source.substr(0, 5));
+BENCHMARK_CAPTURE(hash_string, (std_hash, 6), std_hash_sv, source.substr(0, 6));
+BENCHMARK_CAPTURE(hash_string, (std_hash, 7), std_hash_sv, source.substr(0, 7));
+BENCHMARK_CAPTURE(hash_string, (std_hash, 8), std_hash_sv, source.substr(0, 8));
+BENCHMARK_CAPTURE(hash_string, (std_hash, 9), std_hash_sv, source.substr(0, 9));
+BENCHMARK_CAPTURE(hash_string, (std_hash, 10), std_hash_sv, source.substr(0, 10));
+BENCHMARK_CAPTURE(hash_string, (std_hash, 11), std_hash_sv, source.substr(0, 11));
+BENCHMARK_CAPTURE(hash_string, (std_hash, 12), std_hash_sv, source.substr(0, 12));
+BENCHMARK_CAPTURE(hash_string, (std_hash, 13), std_hash_sv, source.substr(0, 13));
+BENCHMARK_CAPTURE(hash_string, (std_hash, 14), std_hash_sv, source.substr(0, 14));
+BENCHMARK_CAPTURE(hash_string, (std_hash, 15), std_hash_sv, source.substr(0, 15));
+BENCHMARK_CAPTURE(hash_string, (std_hash, 16), std_hash_sv, source.substr(0, 16));
--- include/nav/nav_hashmap.hpp
+++ include/nav/nav_hashmap.hpp
@@ -5,20 +5,7 @@
 
 namespace nav::detail {
 template <uint64_t initial_value, uint64_t mul>
-constexpr auto basic_sv_hash = [](std::string_view sv) -> unsigned {
-    // A prime close to a power of 2 is chosen b/c modulo can be computed with
-    // shifts and additions. The compiler does this automatically.
-    constexpr uint64_t p = (uint64_t(1) << 61) - 1;
-    uint64_t h = initial_value;
-    for (char ch : sv) {
-        h = ((h * mul) + ch) % p;
-    }
-
-    return unsigned(h);
-};
-
-template <uint64_t initial_value, uint64_t mul>
-constexpr auto basic_block_sv_hash = [](std::string_view sv) -> unsigned {
+constexpr auto block8_hash = [](std::string_view sv) -> unsigned {
     // A prime close to a power of 2 is chosen b/c modulo can be computed with
     // shifts and additions. The compiler does this automatically.
     constexpr uint64_t p = (uint64_t(1) << 61) - 1;
@@ -27,26 +14,30 @@ constexpr auto basic_block_sv_hash = [](std::string_view sv) -> unsigned {
     char const* str = sv.data();
     size_t size = sv.size();
 
-    for (char const* end = str + size - size % 4; str != end; str += 4) {
-        unsigned item = unsigned(str[0])       //
-                      | unsigned(str[1]) << 8  //
-                      | unsigned(str[2]) << 16 //
-                      | unsigned(str[3]) << 24;
+    for (char const* end = str + size - size % 8; str != end; str += 8) {
+        uint64_t item = uint64_t(str[0])       //
+                      | uint64_t(str[1]) << 8  //
+                      | uint64_t(str[2]) << 16 //
+                      | uint64_t(str[3]) << 24 //
+                      | uint64_t(str[4]) << 32 //
+                      | uint64_t(str[5]) << 40 //
+                      | uint64_t(str[6]) << 48 //
+                      | uint64_t(str[7]) << 56;
         h = ((h * mul) + item) % p;
     }
 
     // remaining bytes that didn't fit in a block of 4
-    int n = size % 4;
-    unsigned item = 0;
-    for (int i = 0; i < n; i++) {
-        item |= unsigned(str[i]) << (i * 8);
+    int n = size % 8;
+    if (n > 0) {
+        uint64_t item = 0;
+        for (int i = 0; i < n; i++) {
+            item |= uint64_t(str[i]) << (i * 8);
+        }
+        h = ((h * mul) + item) % p;
     }
-    h = ((h * mul) + item) % p;
 
     return unsigned(h);
 };
 
-constexpr auto sv_hash = basic_block_sv_hash<0, 31>;
-constexpr auto block_sv_hash = basic_block_sv_hash<0, 31>;
-
+constexpr auto hash_sv = block8_hash<0, 31>;
 } // namespace nav::detail
